655f2bac32508d40c3c33e247ed68b271f1e9c12
chenyantaoaaa <334632474@qq.com>
2019-07-06 13:55:03 +0800

    first commit

---
+++ src/main/java/com/sxh/user/model/User.java
@@ -0,0 +1,355 @@
+package com.sxh.user.model;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String accountId;
+
+    private String loginName;
+
+    private String loginPassword;
+
+    private String name;
+
+    private String number;
+
+    private Integer groupId;
+
+    private Integer companyId;
+
+    private Integer departmentId;
+
+    private Integer jobId;
+
+    private Byte sex;
+
+    private String idCard;
+
+    private String tel;
+
+    private Date entryDay;
+
+    private Date birthday;
+
+    private String email;
+
+    private Integer leaderId;
+
+    private String leaderName;
+
+    private String officePhone;
+
+    private String officeArea;
+
+    private Integer jobLevel;
+
+    private String remark;
+
+    private Boolean fixeduser;
+
+    private Boolean deleteFlag;
+
+    private Integer sanfangId;
+
+    private String sanfangName;
+
+    private Integer sanfangAccountId;
+
+    private String sanfangAccountName;
+
+    private String sanfangOrgName;
+
+    private Integer creator;
+
+    private String creatorName;
+
+    private Date createTime;
+
+    private Integer modifier;
+
+    private String modifierName;
+
+    private Date modifyTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getLoginPassword() {
+        return loginPassword;
+    }
+
+    public void setLoginPassword(String loginPassword) {
+        this.loginPassword = loginPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Integer getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(Integer jobId) {
+        this.jobId = jobId;
+    }
+
+    public Byte getSex() {
+        return sex;
+    }
+
+    public void setSex(Byte sex) {
+        this.sex = sex;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public Date getEntryDay() {
+        return entryDay;
+    }
+
+    public void setEntryDay(Date entryDay) {
+        this.entryDay = entryDay;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getLeaderId() {
+        return leaderId;
+    }
+
+    public void setLeaderId(Integer leaderId) {
+        this.leaderId = leaderId;
+    }
+
+    public String getLeaderName() {
+        return leaderName;
+    }
+
+    public void setLeaderName(String leaderName) {
+        this.leaderName = leaderName;
+    }
+
+    public String getOfficePhone() {
+        return officePhone;
+    }
+
+    public void setOfficePhone(String officePhone) {
+        this.officePhone = officePhone;
+    }
+
+    public String getOfficeArea() {
+        return officeArea;
+    }
+
+    public void setOfficeArea(String officeArea) {
+        this.officeArea = officeArea;
+    }
+
+    public Integer getJobLevel() {
+        return jobLevel;
+    }
+
+    public void setJobLevel(Integer jobLevel) {
+        this.jobLevel = jobLevel;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public Boolean getFixeduser() {
+        return fixeduser;
+    }
+
+    public void setFixeduser(Boolean fixeduser) {
+        this.fixeduser = fixeduser;
+    }
+
+    public Boolean getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(Boolean deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+
+    public Integer getSanfangId() {
+        return sanfangId;
+    }
+
+    public void setSanfangId(Integer sanfangId) {
+        this.sanfangId = sanfangId;
+    }
+
+    public String getSanfangName() {
+        return sanfangName;
+    }
+
+    public void setSanfangName(String sanfangName) {
+        this.sanfangName = sanfangName;
+    }
+
+    public Integer getSanfangAccountId() {
+        return sanfangAccountId;
+    }
+
+    public void setSanfangAccountId(Integer sanfangAccountId) {
+        this.sanfangAccountId = sanfangAccountId;
+    }
+
+    public String getSanfangAccountName() {
+        return sanfangAccountName;
+    }
+
+    public void setSanfangAccountName(String sanfangAccountName) {
+        this.sanfangAccountName = sanfangAccountName;
+    }
+
+    public String getSanfangOrgName() {
+        return sanfangOrgName;
+    }
+
+    public void setSanfangOrgName(String sanfangOrgName) {
+        this.sanfangOrgName = sanfangOrgName;
+    }
+
+    public Integer getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Integer creator) {
+        this.creator = creator;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    public void setCreatorName(String creatorName) {
+        this.creatorName = creatorName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(Integer modifier) {
+        this.modifier = modifier;
+    }
+
+    public String getModifierName() {
+        return modifierName;
+    }
+
+    public void setModifierName(String modifierName) {
+        this.modifierName = modifierName;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
---
+++ src/main/java/com/sxh/user/service/DemoService.java
@@ -0,0 +1,12 @@
+package com.sxh.user.service;
+
+
+import com.sxh.user.model.User;
+
+/**
+ * @author yang.shang
+ * @create 2019-07-01 17:15
+ **/
+public interface DemoService {
+    User demoMethod(Integer id);
+}
